d6bf5a83eb13e238d3422d70cd2dd8148b3edb95
Brennon Nuckols <brennuck@gmail.com>
2020-03-12 11:37:14 -0600

    Edited /mine endpoint

--- client_mining_p/blockchain.py
+++ client_mining_p/blockchain.py
@@ -127,19 +127,38 @@ print(blockchain.chain)
 print(blockchain.hash(blockchain.last_block))
 
 
-@app.route('/mine', methods=['GET'])
+@app.route('/mine', methods=['POST'])
 def mine():
     # Run the proof of work algorithm to get the next proof
-    proof = blockchain.proof_of_work(blockchain.last_block)
+    # proof = blockchain.proof_of_work(blockchain.last_block)
 
-    # Forge the new Block by adding it to the chain with the proof
-    previous_hash = blockchain.hash(blockchain.last_block)
-    new_block = blockchain.new_block(proof, previous_hash)
+    # # Forge the new Block by adding it to the chain with the proof
+    # previous_hash = blockchain.hash(blockchain.last_block)
+    # new_block = blockchain.new_block(proof, previous_hash)
+    data = request.get_json()
+    required = ['proof', 'id']
+    if not all(i in data for i in required):
+        response = {
+            'message': 'Error!'
+        }
 
-    response = {
-        # TODO: Send a JSON response with the new block
-        "block": new_block
-    }
+        return jsonify(response), 400
+
+    last_block = blockchain.last_block
+    last_block_string = json.dumps(last_block, sort_keys=True)
+
+    if blockchain.valid_proof(last_block_string, data['proof']):
+        previous_hash = blockchain.hash(blockchain.last_block)
+        block = blockchain.new_block(data['proof'], previous_hash)
+
+        response = {
+            "new_block": block,
+            'message': "New block forged"
+        }
+    else:
+        response = {
+            "message": "Error!"
+        }
 
     return jsonify(response), 200
 
